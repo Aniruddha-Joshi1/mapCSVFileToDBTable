64a050b11f4ad4cfa73da9ed2eadf22209231bb9
aniruddha.joshi@kanerika.com-at-972218571398 <aniruddha.joshi@kanerika.com>
2025-04-07 19:40:13 +0530

    intialise repo with standard packages

---
+++ src/main/java/com/learn/csvToDBTable/CsvToDbTableApplication.java
@@ -0,0 +1,13 @@
+package com.learn.csvToDBTable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvToDbTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvToDbTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/csvToDBTable/CsvToDbTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.csvToDBTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvToDbTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
